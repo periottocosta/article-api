b517a13a63f6afd4360e702de9cf77fc7cb5dc0d
Gabriel Periotto Costa <gabriel.costa@hsl.org.br>
2022-02-18 13:07:13 -0300

    Remove Files

--- API/article/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url, include
-
-from . import views
-
-urlpatterns = [
-    path('', views.ArticleList.as_view()),
-    path('/<int:pk>', views.ArticleDetails.as_view())
-]
